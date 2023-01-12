3ee26731c27ecae8b01a3acc30b112d4c751eeb3
NightShadow38 <oceanphoenix888@gmail.com>
2023-01-12 14:08:47 +1100

    Arm | Merge master into arm

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -23,8 +23,7 @@ RobotContainer::RobotContainer() {
 }
 
 void RobotContainer::ConfigureBindings() {
-    //using BtnId = frc::XboxController::Button;
-   // using Btn = frc2::JoystickButton;
+  using namespace frc2::cmd;
  
   // Schedule `ExampleMethodCommand` when the Xbox controller's B button is
   // pressed, cancelling on release.
@@ -34,14 +33,14 @@ void RobotContainer::ConfigureBindings() {
   _driverController.LeftBumper().WhileTrue(cmd::ClawGrabCone());
   _driverController.RightTrigger().WhileTrue(cmd::ClawGrabCube());
 
-  m_driverController.A().OnTrue(RunOnce([]{SubArm::GetInstance().DriveTo(20_deg, -20_deg);}));
-  m_driverController.B().OnTrue(RunOnce([]{SubArm::GetInstance().DriveTo(10_deg, 10_deg);}));
+  _driverController.A().OnTrue(RunOnce([]{SubArm::GetInstance().DriveTo(20_deg, -20_deg);}));
+  _driverController.B().OnTrue(RunOnce([]{SubArm::GetInstance().DriveTo(10_deg, 10_deg);}));
   //ARM_LENGTH = 1m, ARM_LENGTH_2 = 1m
-  m_driverController.X().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.17862_m + SubArm::ARM_ROOT_X, 0.07_m);})); //Cube/Cone low
-  m_driverController.Y().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.222655_m + 0.35724_m + SubArm::ARM_ROOT_X, 0.67_m);})); //Cube mid
-  //m_driverController.X().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.2017795_m + 0.8024369_m + SubArm::ARM_ROOT_X, 0.97_m);})); //Cube high
-  m_driverController.LeftBumper().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.559435_m + SubArm::ARM_ROOT_X, 0.862787_m + 0.07_m);})); //Cone mid
-  m_driverController.RightBumper().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.99156774_m + SubArm::ARM_ROOT_X, 1.1668125_m + 0.07_m);})); //Cone high
+  _driverController.X().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.17862_m + SubArm::ARM_ROOT_X, 0.07_m);})); //Cube/Cone low
+  _driverController.Y().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.222655_m + 0.35724_m + SubArm::ARM_ROOT_X, 0.67_m);})); //Cube mid
+  //_driverController.X().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.2017795_m + 0.8024369_m + SubArm::ARM_ROOT_X, 0.97_m);})); //Cube high
+  _driverController.LeftBumper().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.559435_m + SubArm::ARM_ROOT_X, 0.862787_m + 0.07_m);})); //Cone mid
+  _driverController.RightBumper().OnTrue(RunOnce([]{SubArm::GetInstance().ArmPos(0.99156774_m + SubArm::ARM_ROOT_X, 1.1668125_m + 0.07_m);})); //Cone high
 
 
   _driverController.Start().OnTrue(frc2::cmd::RunOnce([]{SubDriveBase::GetInstance().ResetGyroHeading();}));
--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -4,23 +4,6 @@
 
 #pragma once
 
-<<<<<<< HEAD
-/**
- * The Constants header provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants.  This should not be used for any other
- * purpose.
- *
- * It is generally a good idea to place constants into subsystem- or
- * command-specific namespaces within this header, which can then be used where
- * they are needed.
- */
-
-namespace OperatorConstants {
-
-constexpr int kDriverControllerPort = 0;
-
-}  // namespace OperatorConstants
-=======
 namespace canid {
     // drive base id: 1 - 19
     constexpr int DriveBaseFrontLeftDrive = 1;
@@ -55,4 +38,3 @@ namespace pcm {
 namespace dio {
     constexpr int lineBreakPayload = 0;
 }
->>>>>>> master
