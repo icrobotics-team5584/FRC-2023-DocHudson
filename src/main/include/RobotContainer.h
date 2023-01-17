2dbdd398d49f3f757b25e2b24a68a0976cd71b86
Aaron Kennedy <aaronkennedy1000@gmail.com>
2023-01-17 14:58:16 +1100

    adjusted tank drive

--- src/main/include/RobotContainer.h
+++ src/main/include/RobotContainer.h
@@ -8,6 +8,7 @@
 #include <frc2/command/Command.h>
 #include <frc2/command/button/CommandXboxController.h>
 #include <frc2/command/button/JoystickButton.h>
+#include <frc2/command/button/CommandGenericHID.h> 
 #include <frc2/command/InstantCommand.h>
 #include <frc/XboxController.h>
 
@@ -29,6 +30,7 @@ class RobotContainer {
  private:
   // Replace with CommandPS4Controller or CommandJoystick if needed
   frc2::CommandXboxController _driverController{0};
+  frc2::CommandGenericHID _secondController{1};
   
   void ConfigureBindings();
   
