999de6a42237f9be5fcecebc42850ccc3d54a52a
gastatouille <bouchergaston4@gmail.com>
2023-02-04 15:20:23 +1100

    #31 | Added red alliance score driveposes

--- src/main/cpp/utilities/Grids.cpp
+++ src/main/cpp/utilities/Grids.cpp
@@ -5,15 +5,15 @@
 
 namespace grids {
     std::map <DrivePose, frc::Pose2d> ScoringLocations = {
-        {{Grid::Left, Column::Left}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Left, Column::Middle}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Left, Column::Right}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Middle, Column::Left}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Middle, Column::Middle}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Middle, Column::Right}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Right, Column::Left}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Right, Column::Middle}, {1_m, 4_m, 45_deg}}, 
-        {{Grid::Right, Column::Right}, {1_m, 4_m, 45_deg}} 
+        {{Grid::Left, Column::Left}, {14.74_m, 5.02_m, 0_deg}}, 
+        {{Grid::Left, Column::Middle}, {14.74_m, 4.43_m, 0_deg}}, 
+        {{Grid::Left, Column::Right}, {14.74_m, 3.82_m, 0_deg}}, 
+        {{Grid::Middle, Column::Left}, {1_m, 3.25_m, 0_deg}}, 
+        {{Grid::Middle, Column::Middle}, {14.74_m, 2.73_m, 0_deg}}, 
+        {{Grid::Middle, Column::Right}, {14.74_m, 2.16_m, 0_deg}},
+        {{Grid::Right, Column::Left}, {14.74_m, 1.59_m, 0_deg}}, 
+        {{Grid::Right, Column::Middle}, {14.74_m, 1.06_m, 0_deg}}, 
+        {{Grid::Right, Column::Right}, {14.74_m, 0.43_m, 0_deg}} 
         
         
     };
