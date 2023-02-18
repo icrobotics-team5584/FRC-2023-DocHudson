923d252932a99c0c28ac7d49d765ef4f1b85d1a8
gastatouille <bouchergaston4@gmail.com>
2023-02-18 12:06:39 +1100

    Put absolute encoder to dashboard

--- src/main/cpp/subsystems/SubClaw.cpp
+++ src/main/cpp/subsystems/SubClaw.cpp
@@ -10,16 +10,15 @@ SubClaw::SubClaw() {
      _clawMotor1.SetPIDFF(P, I, D, F);
 
     _clawMotor1.UseAbsoluteEncoder(_clawEncoder);
-
-    _clawMotor1.SetPosition(0_tr);
      
 }
 
-// This method will be called once per scheduler run
+// This method will be called once per scheduler runte
 void SubClaw::Periodic() {
     frc::SmartDashboard::PutNumber("Claw/claw Duty cycle", _clawMotor1.GetAppliedOutput());
     frc::SmartDashboard::PutNumber("Claw/claw Current", _clawMotor1.GetOutputCurrent());
     frc::SmartDashboard::PutNumber("Claw/power", _clawMotor1.GetSimVoltage().value());
+    frc::SmartDashboard::PutNumber("Claw/Encoder Pos", _clawEncoder.GetPosition());
     
 //frc::SmartDashboard::PutNumber("RightClawPneumatc", _solPnuematicsLeft.Get());
 //frc::SmartDashboard::PutNumber("LeftClawPneumatc", _solPnuematicsRight.Get());
