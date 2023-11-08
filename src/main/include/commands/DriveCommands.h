c8274cde1ec3ab949a7078ad921543a2e6c487cd
NightShadow38 <oceanphoenix888@gmail.com>
2023-11-08 21:08:27 +1100

    worked on snap and drive angle

--- src/main/include/commands/DriveCommands.h
+++ src/main/include/commands/DriveCommands.h
@@ -2,6 +2,7 @@
 
 #include <frc/geometry/Pose2d.h>
 #include <frc2/command/Commands.h>
+#include <frc/XboxController.h>
 
 namespace cmd {
   frc2::CommandPtr  AddVisionMeasurement();
@@ -11,7 +12,10 @@ namespace cmd {
 
   //auto balancing
   frc2::CommandPtr AutoBalance();
-
   frc2::CommandPtr DriveBackwards();
+
+  //driving to angle
+  frc2::CommandPtr SnapToAngle();
+  frc2::CommandPtr Drive(frc::XboxController &controller);
   
 }
